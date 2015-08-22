f90b0cac6f18c86b6b6f9147367568c7aad613d1
Amanda Richards <amanda@telmate.com>
2015-08-22 16:56:01 -0700

    Add validations to the user and recipe models.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :instructions, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_many :recipes
+
+  validates :username, presence: true
+  validates :email, presence: true
 end
