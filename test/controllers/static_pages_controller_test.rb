f92e6a857903e50d41785cab8fec470969a6fce6
Amanda Richards <amanda@telmate.com>
2015-08-22 17:15:14 -0700

    Add initial content and tests for about, help, and home pages.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Home Cookin'"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Welcome | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
